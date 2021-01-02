1e9df36f790cfcdba31316f5d76b1e8e121a08fd
Michael Desson <michael.desson@gmail.com>
2021-01-02 14:02:04 -0500

    Basic webserver setup

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
-	"os"
 	"strconv"
 	"strings"
 
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
 
@@ -17,11 +18,17 @@ func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Please include an .env file with SID and TOKEN values from Twilio")
 	}
-	sid := os.Getenv("SID")
-	token := os.Getenv("TOKEN")
-	to := os.Getenv("TO")
-	from := os.Getenv("FROM")
+	// sid := os.Getenv("SID")
+	// token := os.Getenv("TOKEN")
+	// to := os.Getenv("TO")
+	// from := os.Getenv("FROM")
+	r := mux.NewRouter()
+	r.HandleFunc("/sms", exampleHandler).Methods("POST")
+	http.Handle("/", r)
+	http.ListenAndServe(":8080", nil)
+}
 
+func sendText(msg, sid, token, to, from string) int {
 	// Config for text message
 	data := url.Values{}
 	data.Set("To", to)
@@ -40,4 +47,15 @@ func main() {
 	client := &http.Client{}
 	resp, _ := client.Do(r)
 	fmt.Println(resp.StatusCode)
+
+	return resp.StatusCode
+}
+
+// Handlers
+func exampleHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatalf("Error decoding request body:\n%v", err)
+	}
+	log.Println(string(body))
 }
